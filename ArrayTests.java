c32fff15421ca045c02061b857cd8c1c58aeaf8a
Hillary <hillarychang11@gmail.com>
2023-10-23 13:00:08 -0700

    fixes to reverse

--- ArrayTests.java
+++ ArrayTests.java
@@ -2,11 +2,19 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
+
+
+  
 	@Test 
 	public void testReverseInPlace() {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
+
+    int[] input2 = {4, 6, 7, 1};
+    ArrayExamples.reverseInPlace(input2);
+    // System.out.println(ArrayExamples.reverseInPlace(input1));
+    assertArrayEquals(new int[]{1, 7, 6, 4}, input2);
 	}
 
 
@@ -14,5 +22,8 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+
+    int[] input2 = {4, 6, 7, 1};
+    assertArrayEquals(new int[]{1, 7, 6, 4}, ArrayExamples.reversed(input2));
   }
 }
